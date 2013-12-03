db8436c0bc83784e417aaa4c4373e95032b4e6ed
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2013-12-03 15:19:06 -0800

    Adding org-journal, org-pomodoro, and Sage

--- init.el
+++ init.el
@@ -29,6 +29,8 @@
 		      linum-relative
                       magit
                       markdown-mode
+		      org-journal
+		      org-pomodoro
 		      php-mode
                       projectile
                       puppet-mode
@@ -109,10 +111,16 @@
 			  ("reals" "\\mathbb{R}" t "&#8477;" "" "" "ℝ")
 			  ("ints" "\\mathbb{Z}" t "&#8484;" "" "" "ℤ")
 			  ("models" "\\models" nil "&#8872;" "" "" "⊧")))
+(require 'org-journal)
 
 ;;; activate projectile
 (require 'projectile)
 (projectile-global-mode)
+
+;;; sage
+(add-to-list 'load-path "/Applications/Sage.app/Contents/Resources/sage/data/emacs")
+(require 'sage "sage")
+(setq sage-command "~/bin/sage")
 
 ;;; activate smartparens
 (smartparens-global-mode t)
@@ -279,3 +287,15 @@
 (provide 'init)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
