6932f456de302c62c3ea46f7e2ff137334f05254
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2013-12-03 15:18:39 -0800

    Configuring ERC

--- init.el
+++ init.el
@@ -175,6 +175,34 @@
 ;;; disable bell function
 (setq ring-bell-function 'ignore)
 
+;;; erc, configured with help from
+;;;; http://emacs-fu.blogspot.com/2009/06/erc-emacs-irc-client.html
+(require 'erc)
+(erc-autojoin-mode t)
+(setq erc-autojoin-channels-alist
+  '((".*\\.freenode.net" "#emacs" "#gnu" "#gcc")))
+
+;; check channels
+(erc-track-mode t)
+(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+				"324" "329" "332" "333" "353" "477"))
+;; don't show any of this
+(setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
+
+;; start or switch to buffer function
+(defun erc-start-or-switch ()
+  "Connect to ERC, or switch to last active buffer"
+  (interactive)
+  (if (get-buffer "irc.freenode.net:6667") ;; ERC already active?
+
+    (erc-track-switch-buffer 1) ;; yes: switch to last active
+    (when (y-or-n-p "Start ERC? ") ;; no: maybe start ERC
+      (erc :server "irc.freenode.net" :port
+      6667 :nick "andschwa" :full-name "Andrew Schwartzmeyer"))))
+
+;; switch to ERC with Ctrl+c e
+(global-set-key (kbd "C-c C-e") 'erc-start-or-switch)
+
 ;;; eval-buffer
 (global-set-key (kbd "C-c C-x") 'eval-buffer)
 
