dc9f72e937a7dc11718ea4be0784e43f2a5e0a7d
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-05-17 20:24:35 -0700

    Setting cursor back to box (default)

--- init.el
+++ init.el
@@ -37,7 +37,6 @@
 (setq calendar-week-start-day 1)
 ;; cursor settings
 (blink-cursor-mode t)
-(setq-default cursor-type 'hbar)
 ;; visually wrap lines
 (setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
 ;; default truncate lines
