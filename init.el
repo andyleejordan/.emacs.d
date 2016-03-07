13a5d607cbc5f4a7f5de76506dabfdb6fccae890
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2016-03-06 18:00:32 -0800

    Add evil-smartparens

--- init.el
+++ init.el
@@ -361,6 +361,8 @@
 (use-package smartparens
   :diminish smartparens-mode
   :init
+  (use-package evil-smartparens
+    :config (add-hook 'smartparens-enabled-hook #'evil-smartparens-mode))
   (require 'smartparens-config)
   (smartparens-global-mode)
   (show-smartparens-global-mode))
