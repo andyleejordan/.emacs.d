e548e597ed0807f1889b78da283b1ee5697ed1a2
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2018-02-16 19:12:27 -0800

    Add Windows performance tweaks from irony

--- lisp/windows.el
+++ lisp/windows.el
@@ -6,5 +6,9 @@
 ;;; Code:
 (set-face-attribute 'default nil :family "Consolas" :height 100)
 
+;; Windows performance tweaks
+(when (boundp 'w32-pipe-read-delay)
+  (setq w32-pipe-read-delay 0))
+
 (provide 'windows)
 ;;; windows.el ends here
