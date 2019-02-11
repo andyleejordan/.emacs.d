a62f13e173c334eb531b36267d9d5fd0ca008e72
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2019-02-11 13:00:50 -0800

    Use combine-and-quote-strings instead of concat

--- lisp/compile-commands.el
+++ lisp/compile-commands.el
@@ -16,21 +16,23 @@ With ARG, clean first. CMD is one of `configure', `compile', or `test'."
                        (expand-file-name (read-directory-name "Build directory? " root) root))))
   (when (or arg (eq cmd 'clean)) (delete-directory dir t))
   (make-directory dir t)
-  (compile
-   ;; TODO: Use a join function join these with ` && ' instead of this
-   ;; terrible `concat'.
-   (concat
-    "cd " dir
-    (when (member cmd (list 'configure 'compile 'test))
-      (let ((root (cdr (project-current)))
-            ;; TODO: Check for SGX support.
-            (sgx nil))
-        (concat " && cmake " root " -GNinja -DUSE_LIBSGX=" (if sgx "ON" "OFF"))))
-    (when (member cmd (list 'compile 'test))
-      (concat " && ninja -v"))
-    (when (eq cmd 'test)
-      (let ((re (completing-read "Regex? " (list "^edger8r_" "signedness"))))
-        (concat " && ctest -V -R " re))))))
+  (when (eq cmd 'clean) (return))
+  (let ((compile-command
+         ;; TODO: Use a join function join these with ` && ' instead
+         ;; of this terrible `concat'.
+         (combine-and-quote-strings
+          (remove nil `(,(concat "cd " (abbreviate-file-name dir)) ; TODO: Make this work with Tramp.
+                        ,(when (member cmd (list 'configure 'compile 'test))
+                           (let ((root (cdr (project-current)))
+                                 ;; TODO: Check for SGX support.
+                                 (sgx nil))
+                             (format "cmake %s -GNinja -DUSE_LIBSGX=%s" root (if sgx "ON" "OFF"))))
+                        ,(when (member cmd (list 'compile 'test)) "ninja -v")
+                        ,(when (eq cmd 'test)
+                           (let ((re (completing-read "Regex? " (list "^edger8r_" "signedness"))))
+                             (concat "ctest -V -R " re)))))
+          " && ")))
+    (call-interactively 'compile)))
 
 (provide 'compile-commands)
 ;;; compile-commands.el ends here
