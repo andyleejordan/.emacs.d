792aaa38fea2831b678cb4afac8d65d10fe62197
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2019-02-13 10:54:14 -0800

    Replace return with an unless

--- lisp/compile-commands.el
+++ lisp/compile-commands.el
@@ -16,20 +16,20 @@ With ARG, clean first. CMD is one of `configure', `compile', or `test'."
                        (expand-file-name (read-directory-name "Build directory? " root) root))))
   (when (or arg (eq cmd 'clean)) (delete-directory dir t))
   (make-directory dir t)
-  (when (eq cmd 'clean) (return))
-  (let ((default-directory dir) ; Using this instead of `cd' works with Tramp.
-        (compile-command
-         (combine-and-quote-strings
-          (remove nil `(,(when (member cmd (list 'configure 'compile 'test))
-                           (let ((root (file-relative-name (cdr (project-current)) dir))
-                                 (sgx (file-exists-p (concat (file-remote-p default-directory) "/dev/sgx"))))
-                             (format "cmake %s -GNinja -DUSE_LIBSGX=%s" root (if sgx "ON" "OFF"))))
-                        ,(when (member cmd (list 'compile 'test)) "ninja -v")
-                        ,(when (eq cmd 'test) ; TODO: `defvar' a list of test expressions.
-                           (let ((re (completing-read "Regex? " (list "^edger8r_" "signedness"))))
-                             (concat "ctest -V -R " re))))) ; TODO: Set OE_SIMULATION based on `sgx'
-          " && ")))
-    (call-interactively 'compile)))
+  (unless (eq cmd 'clean)
+    (let ((default-directory dir) ; Using this instead of `cd' works with Tramp.
+          (compile-command
+           (combine-and-quote-strings
+            (remove nil `(,(when (member cmd (list 'configure 'compile 'test))
+                             (let ((root (file-relative-name (cdr (project-current)) dir))
+                                   (sgx (file-exists-p (concat (file-remote-p default-directory) "/dev/sgx"))))
+                               (format "cmake %s -GNinja -DUSE_LIBSGX=%s" root (if sgx "ON" "OFF"))))
+                          ,(when (member cmd (list 'compile 'test)) "ninja -v")
+                          ,(when (eq cmd 'test) ; TODO: `defvar' a list of test expressions.
+                             (let ((re (completing-read "Regex? " (list "^edger8r_" "signedness"))))
+                               (concat "ctest -V -R " re))))) ; TODO: Set OE_SIMULATION based on `sgx'
+            " && ")))
+      (call-interactively 'compile))))
 
 (provide 'compile-commands)
 ;;; compile-commands.el ends here
