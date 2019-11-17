5d802da8e7405fb732dc6b4d8b8408c8d8f317a0
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2019-11-16 22:24:45 -0800

    Swap to solarized-light by default

--- init.el
+++ init.el
@@ -698,12 +698,10 @@ Pass APPEND and COMPARE-FN to each invocation of `add-to-list'."
      ((member 'solarized-light custom-enabled-themes)
       (disable-theme 'solarized-light)
       (load-theme 'solarized-dark t))))
-  (load-theme 'solarized-dark t))
+  (load-theme 'solarized-light t))
 
 (when (equal (system-name) "andschwa-oe")
-  (set-face-attribute 'default nil :family "Ubuntu Mono" :height 160)
-  (disable-theme 'solarized-dark)
-  (load-theme 'solarized-light t))
+  (set-face-attribute 'default nil :family "Ubuntu Mono" :height 160))
 
 (unless (display-graphic-p)
   (load-theme 'tango-dark t))
