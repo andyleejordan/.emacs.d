8126cde0421ef20286b36aa2f1124b9fa5d35ec1
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2019-11-25 18:34:42 +0000

    Add `auto-correct-abbrev-table`
    This is useful to share across machines.

---
+++ var/abbrev.el
@@ -0,0 +1,20 @@
+;;-*-coding: utf-8;-*-
+(define-abbrev-table 'auto-correct-abbrev-table
+  '(
+    ("Cmake" "CMake" nil :count 1)
+    ("ammend" "amend" nil :count 1)
+    ("anamolus" "anomalous" nil :count 1)
+    ("cmate" "cmake" nil :count 1)
+    ("docuementation" "documentation" nil :count 1)
+    ("evalute" "evaluate" nil :count 1)
+    ("expressable" "expressible" nil :count 1)
+    ("ignoreing" "ignoring" nil :count 1)
+    ("intrdouction" "introduction" nil :count 1)
+    ("oedger8r" "oeedger8r" nil :count 1)
+    ("propogated" "propagated" nil :count 1)
+    ("remants" "remnants" nil :count 1)
+    ("responsibilites" "responsibilities" nil :count 1)
+    ("unecessarily" "unnecessarily" nil :count 1)
+    ("unncessary" "unnecessary" nil :count 1)
+   ))
+
