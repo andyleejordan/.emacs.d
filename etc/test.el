ebde610ea29ab503ff7e9c3d4c266f95adfb106b
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-05-15 21:13:41 -0700

    Improve test file

--- etc/test.el
+++ etc/test.el
@@ -23,17 +23,33 @@
 ;; Run with `emacs -Q -l test.el'.
 
 ;;; Code:
+(message (emacs-version))
+(toggle-debug-on-error)
 
-;; Typical `package.el' setup with Melpa.
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-(package-refresh-contents)
+;; Typical `package.el' setup with Melpa:
+
+;; (require 'package)
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/") t)
+;; (package-initialize)
+;; (package-refresh-contents)
+
+;; Or typical `straight.el' setup:
+
+;; (defvar bootstrap-version)
+;; (let ((bootstrap-file
+;;        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+;;       (bootstrap-version 5))
+;;   (unless (file-exists-p bootstrap-file)
+;;     (with-current-buffer
+;;         (url-retrieve-synchronously
+;;          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+;;          'silent 'inhibit-cookies)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp)))
+;;   (load bootstrap-file nil 'nomessage))
 
 ;; Insert repro code here:
-
-;; E.g. `(package-install 'foo)'
 
 (provide 'test)
 ;;; test.el ends here
